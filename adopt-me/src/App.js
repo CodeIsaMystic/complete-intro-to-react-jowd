7fed68c0dc6f78d4fb060bac6807ebf2c808146e
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-26 16:29:33 +0100

    convert App in jsx

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -4,6 +4,7 @@ import  Pet  from './Pet';
 
 
 const App = () => {
+  /*
   return React.createElement(
     "div",
     {},
@@ -27,6 +28,18 @@ const App = () => {
       }),
     ]
   );
+  */
+ 
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  )
+
+
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
